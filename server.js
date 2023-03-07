4a6072a7d1818c698202e43c6cd8de6b91080762
wuhibe <wuhibetamire710@gmail.com>
2023-03-07 09:23:46 +0300

    chore: add response to get / route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app.get('/setWebHook', setWebhook);
 
 app.get('/bot', getMe);
